d5908de97dd34c75d628645c7464129b8ff34e42
Jacob Marsh <51686617+jacobimarsh@users.noreply.github.com>
2023-05-30 14:15:35 -0400

    Update notes.sh

--- baseline/notes.sh
+++ baseline/notes.sh
@@ -3,7 +3,7 @@ grep -v MULTIALLELIC test.vcf >> bial_test.vcf
 #OR
 ../../relate_v1.1.9_x86_64_dynamic/bin/RelateFileFormats --mode ConvertFromVcf --haps bial_test.haps --sample bial_test.sample -i bial_test
 
-../../relate_v1.1.9_MacOSX_M1/bin/Relate --mode All -m 1.25e-6 -N 2000 --haps bial_test.haps --sample bial_test.sample --map test.map --seed 1 -o bial_test
+../../relate_v1.1.9_MacOSX_M1/bin/Relate --mode All -m 1.25e-6 -N 4000 --haps bial_test.haps --sample bial_test.sample --map test.map --seed 1 -o bial_test
 
 ../../relate_v1.1.9_MacOSX_M1/scripts/EstimatePopulationSize/EstimatePopulationSize.sh \
               -i bial_test \
