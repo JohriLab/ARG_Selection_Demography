d0388f43b759967727a011968c13085418d20f85
Jacob Ian Marsh <jimarsh@longleaf-login4.its.unc.edu>
2024-02-07 11:43:02 -0500

    add R

--- scripts/dm_pda.sh
+++ scripts/dm_pda.sh
@@ -6,16 +6,26 @@ for x in {mid,high}
 do
 for q in {constant,expansion,contract,bottleneck,repbneck}
 do
-echo -e "production_BGS_${x}S_${y}G_${q}"
-grep -ae p1 /nas/longleaf/home/jimarsh/LOGFILES/production_BGS_${x}S_${y}G_${q}.o | awk '{if ($NF >= 20000) print}' | grep "m." | sed 's/ [^ ]* [^ ]* p1.*//' | sed 's/.*m/m/' | grep -wf /work/users/j/i/jimarsh/drosoph_sims/exon_positions.txt - | sed 's/ .*//' | sort | uniq -c | awk '{if ($2 == "m1") $1 = $1*.4; print}' > mcount_production_BGS_${x}S_${y}G_${q}.txt
-grep m6 mcount_production_BGS_${x}S_${y}G_${q}.txt | sed "s/ //g" | sed "s/m6//g" | xargs -I [] awk '{sum+=$1;} END {print []/sum}' mcount_production_BGS_${x}S_${y}G_${q}.txt
+echo -e "production5_BGS_${x}S_${y}G_${q}"
+grep -ae p1 /nas/longleaf/home/jimarsh/LOGFILES/production5_BGS_${x}S_${y}G_${q}.o | awk '{if ($NF >= 33333) print}' | grep "m." | sed 's/ [^ ]* [^ ]* p1.*//' | sed 's/.*m/m/' | grep -wf /work/users/j/i/jimarsh/drosoph_sims/exon5_positions.txt - | sed 's/ .*//' | sort | uniq -c | awk '{if ($2 == "m1") $1 = 0; print}' > mcount_production5_BGS_${x}S_${y}G_${q}.txt
+grep m6 mcount_production5_BGS_${x}S_${y}G_${q}.txt | sed "s/ //g" | sed "s/m6//g" | xargs -I [] awk '{sum+=$1;} END {print []/sum}' mcount_production5_BGS_${x}S_${y}G_${q}.txt
 done
 done
 done
+
+for q in {constant,expansion,contract,bottleneck,repbneck}
+do
+echo -e "production5_BGS_jensen_${q}"
+grep -ae p1 /nas/longleaf/home/jimarsh/LOGFILES/production5_BGS_jensen_${q}.o | awk '{if ($NF >= 33333) print}' | grep "m." | sed 's/ [^ ]* [^ ]* p1.*//' | sed 's/.*m/m/' | grep -wf /work/users/j/i/jimarsh/drosoph_sims/exon5_positions.txt - | sed 's/ .*//' | sort | uniq -c | awk '{if ($2 == "m1") $1 = 0; print}' > mcount_production5_BGS_jensen_${q}.txt
+grep m6 mcount_production5_BGS_jensen_${q}.txt | sed "s/ //g" | sed "s/m6//g" | xargs -I [] awk '{sum+=$1;} END {print []/sum}' mcount_production5_BGS_jensen_${q}.txt
+done
+
+
+
 
 ##Div in neutral interg/intron regions
 
-#3732050 is length of non-selected interg/intron region * 10 replicates
+#17765900 is length of non-selected interg/intron region * 10 replicates
 
 for y in {low,mid}
 do
@@ -23,18 +33,18 @@ do
 	do
 		for q in {constant,expansion,contract,bottleneck,repbneck}
 		do
-echo -e "production_BGS_${x}S_${y}G_${q}"
-grep -ae m5 /nas/longleaf/home/jimarsh/LOGFILES/production_BGS_${x}S_${y}G_${q}.o | awk '{if ($NF >= 100000) print}' | grep p1 | wc -l | xargs -I [] echo [] "/3732050" | bc -l
+echo -e "production5_BGS_${x}S_${y}G_${q}"
+grep -ae m5 /nas/longleaf/home/jimarsh/LOGFILES/production5_BGS_${x}S_${y}G_${q}.o | awk '{if ($NF >= 33333) print}' | wc -l | xargs -I [] echo [] "/17765900" | bc -l
 		done
 	done
 done
 
-for y in {BGS_nopos,neutral,neutral_nomaps}
+for y in {BGS_nopos,BGS_jensen,neutral,neutral_nomaps}
 do
 	for q in {constant,expansion,contract,bottleneck,repbneck}
 	do
-echo -e "production_${y}_${q}"
-grep -ae m5 /nas/longleaf/home/jimarsh/LOGFILES/production_${y}_${q}.o | awk '{if ($NF >= 100000) print}' | grep p1 | wc -l | xargs -I [] echo [] "/3732050" | bc -l
+echo -e "production5_${y}_${q}"
+grep -ae m5 /nas/longleaf/home/jimarsh/LOGFILES/production5_${y}_${q}.o | awk '{if ($NF >= 33333) print}' | wc -l | xargs -I [] echo [] "/17765900" | bc -l
 	done
 done
 
@@ -46,20 +56,20 @@ do
         do
                 for q in {constant,expansion,contract,bottleneck,repbneck}
                 do
-echo -e "production_BGS_${x}S_${y}G_${q}"
-grep -ae p1 /nas/longleaf/home/jimarsh/LOGFILES/production_BGS_${x}S_${y}G_${q}.o | awk '{if ($NF >= 100000) print}' | sed 's/.*m. //' | sed 's/ .*//' > subs_production_BGS_${x}S_${y}G_${q}.txt
-grep -wf exon_positions.txt subs_production_BGS_${x}S_${y}G_${q}.txt | wc -l | xargs -I [] echo "[]/(5*314*10*113)" | bc -l
+echo -e "production5_BGS_${x}S_${y}G_${q}"
+grep -ae p1 /nas/longleaf/home/jimarsh/LOGFILES/production5_BGS_${x}S_${y}G_${q}.o | awk '{if ($NF >= 33333) print}' | sed 's/.*m. //' | sed 's/ .*//' > subs_production5_BGS_${x}S_${y}G_${q}.txt
+grep -wf exon5_positions.txt subs_production5_BGS_${x}S_${y}G_${q}.txt | wc -l | xargs -I [] echo "[]/(5*314*10*565)" | bc -l
 		done
 	done
 done
 
-for y in {BGS_nopos,neutral,neutral_nomaps}
+for y in {BGS_nopos,BGS_jensen,neutral,neutral_nomaps}
 do
         for q in {constant,expansion,contract,bottleneck,repbneck}
         do
-echo -e "production_${y}_${q}"
-grep -ae p1 /nas/longleaf/home/jimarsh/LOGFILES/production_${y}_${q}.o | awk '{if ($NF >= 100000) print}' | sed 's/.*m. //' | sed 's/ .*//' > subs_production_${y}_${q}.txt
-grep -wf exon_positions.txt subs_production_${y}_${q}.txt | wc -l | xargs -I [] echo "[]/(5*314*10*113)" | bc -l
+echo -e "production5_${y}_${q}"
+grep -ae p1 /nas/longleaf/home/jimarsh/LOGFILES/production5_${y}_${q}.o | awk '{if ($NF >= 33333) print}' | sed 's/.*m. //' | sed 's/ .*//' > subs_production5_${y}_${q}.txt
+grep -wf exon5_positions.txt subs_production5_${y}_${q}.txt | wc -l | xargs -I [] echo "[]/(5*314*10*565)" | bc -l
 	done
 done
 
@@ -72,24 +82,24 @@ do
         do
                 for q in {constant,expansion,contract,bottleneck,repbneck}
                 do
-echo -e "production_BGS_${x}S_${y}G_${q}"
-grep -ae "Rep." /nas/longleaf/home/jimarsh/LOGFILES/production_BGS_${x}S_${y}G_${q}.o | sed 's/.*= //' | sed 's/"//' | awk '{ tot+=$1;cnt++ } END { print tot/cnt }'
+echo -e "production5_BGS_${x}S_${y}G_${q}"
+grep -ae "Rep." /nas/longleaf/home/jimarsh/LOGFILES/production5_BGS_${x}S_${y}G_${q}.o | sed 's/.*= //' | sed 's/"//' | awk '{ tot+=$1;cnt++ } END { print tot/cnt }'
 		done
 	done
 done
 
-for y in {BGS_nopos,neutral,neutral_nomaps}
+for y in {BGS_nopos,BGS_jensen,neutral,neutral_nomaps}
 do
         for q in {constant,expansion,contract,bottleneck,repbneck}
         do
-echo -e "production_${y}_${q}"
-grep -ae "Rep." /nas/longleaf/home/jimarsh/LOGFILES/production_${y}_${q}.o | sed 's/.*= //' | sed 's/"//' | awk '{ tot+=$1;cnt++ } END { print tot/cnt }'
+echo -e "production5_${y}_${q}"
+grep -ae "Rep." /nas/longleaf/home/jimarsh/LOGFILES/production5_${y}_${q}.o | sed 's/.*= //' | sed 's/"//' | awk '{ tot+=$1;cnt++ } END { print tot/cnt }'
 	done
 done
 
 ##Pi in exon regions
 
-#grep -ae g3 production_neutral_nomaps_bottleneck.o | sort | uniq | sed 's/.*g3, /1\t/' | sed 's/, /\t/' | sed 's/).*//' | sort -nk2 > slim_exons.bed
+#grep -ae g3 production5_neutral_nomaps_bottleneck.o | sort | uniq | sed 's/.*g3, /1\t/' | sed 's/, /\t/' | sed 's/).*//' | sort -nk2 > slim_exons.bed
 
 #module load samtools
 #module load vcftools
@@ -104,30 +114,30 @@ done
 #echo ${q}
 #			for i in {1..10}
 #			do
-#bgzip ../production_BGS_${x}S_${y}G_${q}_R${i}.vcf
-#bcftools index ../production_BGS_${x}S_${y}G_${q}_R${i}.vcf.gz
-#bedtools intersect -a ../production_BGS_${x}S_${y}G_${q}_R${i}.vcf.gz -b ../../slim_exons.bed -header > g3_${i}.vcf
+#bgzip ../production5_BGS_${x}S_${y}G_${q}_R${i}.vcf
+#bcftools index ../production5_BGS_${x}S_${y}G_${q}_R${i}.vcf.gz
+#bedtools intersect -a ../production5_BGS_${x}S_${y}G_${q}_R${i}.vcf.gz -b ../../slim_exons.bed -header > g3_${i}.vcf
 #vcftools --vcf g3_${i}.vcf --site-pi --out g3_${i}
-#awk '{ sum += $NF } END { print sum / (5*314*10*113)}' g3_${i}.sites.pi >> production_BGS_${x}S_${y}G_${q}.exon.pi
+#awk '{ sum += $NF } END { print sum / (5*314*10*113)}' g3_${i}.sites.pi >> production5_BGS_${x}S_${y}G_${q}.exon.pi
 #			done
 #		done
 #	done
 #done
 #
-#for y in {BGS_nopos,neutral,neutral_nomaps}
+#for y in {BGS_nopos,BGS_jensen,neutral,neutral_nomaps}
 #do
 #	for q in {constant,expansion,contract,bottleneck,repbneck}
 #        do
 #echo ${q}
 #                for i in {1..10}
 #                do
-#bgzip ../production_${y}_${q}_R${i}.vcf
-#bcftools index ../production_${y}_${q}_R${i}.vcf.gz
-#bedtools intersect -a ../production_${y}_${q}_R${i}.vcf.gz -b ../../slim_exons.bed -header > g3_${i}.vcf
+#bgzip ../production5_${y}_${q}_R${i}.vcf
+#bcftools index ../production5_${y}_${q}_R${i}.vcf.gz
+#bedtools intersect -a ../production5_${y}_${q}_R${i}.vcf.gz -b ../../slim_exons.bed -header > g3_${i}.vcf
 #vcftools --vcf g3_${i}.vcf --site-pi --out g3_${i}
-#awk '{ sum += $NF } END { print sum / (5*313*113)}' g3_${i}.sites.pi >> production_${y}_${q}.exon.pi
+#awk '{ sum += $NF } END { print sum / (5*313*113)}' g3_${i}.sites.pi >> production5_${y}_${q}.exon.pi
 #                done
 #        done
 #done
 #
-#lh | grep production.*pi | sed 's/.* //' | while read line; do echo ${line}; awk '{ tot+=$1;cnt++ } END { print tot/cnt }' ${line}; done
+#lh | grep production5.*pi | sed 's/.* //' | while read line; do echo ${line}; awk '{ tot+=$1;cnt++ } END { print tot/cnt }' ${line}; done
--- scripts/new_build_mask.sh
+++ scripts/new_build_mask.sh
@@ -6,12 +6,11 @@ VCF=${1?Error: no input VCF (e.g. mask_BGS_nopos_constant_R9)}
 END=${2?Error: end of genomic interval not specified (e.g. 1040001 assumes starts at position 1 and ends +1)}
 REGIONS=${3?Error: regions to be masked not specified (e.g. exon_UTR_pos.txt see hashed comments below)} 
 
-#grep -ae "initializeGenomicElement(g4" mask_BGS_nopos_constant.o | sort | uniq > UTR.temp
-#grep -ae "initializeGenomicElement(g5" mask_BGS_nopos_constant.o | sort | uniq >> UTR.temp
-#sed 's/.*g...//' UTR.temp | sed 's/,//' | sed 's/).*//' | sort -nk1 > UTR.txt
-## Already 1-base for exons (2col_positions.txt)
-#./print_nums.sh 2col_positions.txt > exon_positions.txt
-#./print_nums.sh UTR.txt | awk '$1 = $1 + 1 {print $0}' > UTR_positions.txt
+#grep -ae "initializeGenomicElement(g3" /nas/longleaf/home/jimarsh/LOGFILES/production5_BGS_midS_lowG_constant.o | sort | uniq > exon5_UTR.temp
+#grep -ae "initializeGenomicElement(g4" /nas/longleaf/home/jimarsh/LOGFILES/production5_BGS_midS_lowG_constant.o | sort | uniq >> exon5_UTR.temp
+#grep -ae "initializeGenomicElement(g5" /nas/longleaf/home/jimarsh/LOGFILES/production5_BGS_midS_lowG_constant.o | sort | uniq >> exon5_UTR.temp
+#sed 's/.*g...//' exon5_UTR.temp | sed 's/,//' | sed 's/).*//' | sort -nk1 > exon5_UTR.txt
+#./dfe_productionbox/alphabox/print_nums.sh bin/exon5_UTR.txt | awk '$1 = $1 + 1 {print $0}' > exon5_UTR_pos.txt
 
 grep MT= $1.vcf | grep -v MT=5 | awk '{print $2}' > mno5_$1.sites
 python -c "for i in range(1, $2): print(i)" | cat - $3 mno5_$1.sites | sort | uniq -c | sort -k2 -n > $1_counts.txt
